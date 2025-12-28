69370fd8f547166f648b30ea95939865b2445807
xxaqurm <tohanesterov003@gmail.com>
2025-12-28 16:17:41 +0700

    Remove benchmark serialization

---
+++ C++/benchmark.hpp
@@ -0,0 +1,87 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <chrono>
+
+using namespace std;
+
+template<typename Func>
+long long benchmark(Func f) {
+    auto start = std::chrono::high_resolution_clock::now();
+    f();
+    auto end = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+}
+
+template <typename DS>
+int runDSBenchmark(const string& operation, const vector<int>& data, int n,
+                 long long& timeOnce, long long& timeSeries)
+{
+    DS ds;
+
+    if (operation == "find" || operation == "remove") {
+        for (auto x : data) ds.push_back(x);
+    }
+
+    if (operation == "insert") {
+        timeSeries = benchmark([&]() {
+            for (auto x : data) ds.push_back(x);
+        });
+    }
+    else if (operation == "find") {
+        int target = ds.at(n / 2);
+
+        timeOnce = benchmark([&]() { ds.find(target); });
+
+        timeSeries = benchmark([&]() {
+            for (auto x : data) ds.find(x);
+        });
+    }
+    else if (operation == "remove") {
+        int target = ds.at(n / 2);
+
+        timeOnce = benchmark([&]() { ds.remove(target); });
+
+        timeSeries = benchmark([&]() {
+            for (auto x : data) ds.remove(x);
+        });
+    }
+    else {
+        throw runtime_error("Неизвестная операция: " + operation);
+    }
+
+    return 0;
+}
+
+template <typename DS>
+int runHashBenchmark(const string& operation, const vector<int>& data, int n,
+                 long long& timeOnce, long long& timeSeries)
+{
+    DS ds;
+
+    if (operation == "find" || operation == "remove") {
+        for (auto x : data) ds.insert(x);
+    }
+
+    if (operation == "insert") {
+        timeSeries = benchmark([&]() {
+            for (auto x : data) ds.insert(x);
+        });
+    }
+    else if (operation == "find") {
+        timeSeries = benchmark([&]() {
+            for (auto x : data) ds.contains(x);
+        });
+    }
+    else if (operation == "remove") {
+        timeSeries = benchmark([&]() {
+            for (auto x : data) ds.remove(x);
+        });
+    }
+    else {
+        throw runtime_error("Неизвестная операция: " + operation);
+        return 1;
+    }
+
+    return 0;
+}
